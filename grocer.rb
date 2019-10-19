df300a24124fbd9a2eb72a53222aff8e1c7c2fd3
Wills Blake <richwblake@gmail.com>
2019-10-19 21:00:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -98,5 +98,6 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
   checkout = consolidate_cart( cart )
+  checkout = apply_coupons( checkout )
   
 end
