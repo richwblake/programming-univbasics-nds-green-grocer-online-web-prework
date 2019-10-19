1b9f61a60004dabde3c41fb4f48359866dc073b2
Wills Blake <richwblake@gmail.com>
2019-10-19 21:03:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -107,6 +107,7 @@ def checkout(cart, coupons)
   while index < checkout.size do
     current_item_total = checkout[index][:price] * checkout[index][:count]
     current_item_total.round(2)
+    grand_total += current_item_total
     index += 1
   end
 end
